6c9eca73b99fa5b8c1dfd75612b094013f13f71b
Timothy Martinez <tmrcmartinez@gmail.com>
2019-08-06 09:42:36 -0400

    added plants and products models

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_130546) do
+ActiveRecord::Schema.define(version: 2019_08_06_134103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
-    t.integer "size"
     t.integer "light"
+    t.integer "size"
     t.integer "maintenance"
     t.integer "category"
     t.string "description"
