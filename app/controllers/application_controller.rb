1dfe333d593205b5843cfec94809610bb1275c61
Timothy Martinez <tmrcmartinez@gmail.com>
2019-08-06 13:09:26 -0400

    updated user authentication. working routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!, only: [:show, :edit, :destroy, :update] #you would put this in the UsersController
+  
 end
