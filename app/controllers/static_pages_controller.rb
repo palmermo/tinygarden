42ef7b67cc7763861d450ffceb5e35c8c5f70df9
Matthew <matthew.o.palm@gmail.com>
2019-08-11 16:36:36 -0400

    social icons link to external sites

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
   before_action :login_instagram, only: [:landing]
 
-  def landing
-    @plants = Plant.all
-    @insta_feed = Instagram.client(:access_token => session[:access_token]).user_recent_media.shuffle.sample(4)
-  end
+  # def landing
+  #   @plants = Plant.all
+  #   @insta_feed = Instagram.client(:access_token => session[:access_token]).user_recent_media.shuffle.sample(4)
+  # end
 
   def care
   end
