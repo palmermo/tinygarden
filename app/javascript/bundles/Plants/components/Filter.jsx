2a89afbd8979dfaa3253047de0d47718438ab4e3
Israel Martinez <izzycode@users.noreply.github.com>
2019-08-12 13:30:27 -0400

    Update Filter.jsx

--- app/javascript/bundles/Plants/components/Filter.jsx
+++ app/javascript/bundles/Plants/components/Filter.jsx
@@ -4,10 +4,10 @@ const Filter = ({ filterType, filterParams = [], handleClick }) => (
   <div>
     <h1>{filterType}</h1>
     {filterParams.map(({ value, label }) => (
-      <>
+      <React.Fragment key={label}>
         <label>{label}</label>
         <input type="checkbox" value={value} onClick={() => handleClick(filterType, value)} />
-      </>
+      </React.Fragment>
     ))}
   </div>
 )
