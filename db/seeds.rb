879908f3bffddc860c76ec82c04feb630450d77d
Timothy Martinez <tmrcmartinez@gmail.com>
2019-08-19 11:21:50 -0400

    cart and design refinement

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,19 +37,19 @@ Product.create([
   {sku: "mon10", price: 1000, sellable_type: "Plant", sellable_id: Plant.find_by(name: 'Monstera').id, amount: rand(25) }
 ])
 
+Product.all.sample(20).each{ |p| CartProduct.create( cart_id: user.cart.id, product_id: p.id , amount: rand(25) )}
+
 Accessory.create([
   {name: 'Pot', description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.", variant: [0,1,2].sample, url: "http://tinygarden-bucket.herokuapp.com/images/Accessories/table.png" },
-  {name: 'table', description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.", variant: [0,1,2].sample, url: "http://tinygarden-bucket.herokuapp.com/images/Accessories/tall-table.png" },
-  {name: 'hanging thing', description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.", variant: [0,1,2].sample, url: "http://tinygarden-bucket.herokuapp.com/images/Accessories/hanging-pot.png" },
+  {name: 'Table', description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.", variant: [0,1,2].sample, url: "http://tinygarden-bucket.herokuapp.com/images/Accessories/tall-table.png" },
+  {name: 'Hanging thing', description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.", variant: [0,1,2].sample, url: "http://tinygarden-bucket.herokuapp.com/images/Accessories/hanging-pot.png" },
   {name: 'Shelf', description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.", variant: [0,1,2].sample, url: "http://tinygarden-bucket.herokuapp.com/images/Accessories/shelf.png" }
 ])
 
 
 Product.create([
-  {sku: "ch1", price: 4000, sellable_type: "Accessory", sellable_id: Accessory.find_by(name: 'chair').id, amount: rand(25) },
-  {sku: "tab2", price: 10000, sellable_type: "Accessory", sellable_id: Accessory.find_by(name: 'table').id, amount: rand(25) },
-  {sku: "thingy42", price: 3400, sellable_type: "Accessory", sellable_id: Accessory.find_by(name: 'hanging thing').id, amount: rand(25) }
-  ])
-
-  
-  Product.all.sample(20).each{ |p| CartProduct.create( cart_id: user.cart.id, product_id: p.id , amount: rand(25) )}
+  {sku: "pot1", price: 4000, sellable_type: "Accessory", sellable_id: Accessory.find_by(name: 'Pot').id, amount: rand(25) },
+  {sku: "shelf1", price: 4000, sellable_type: "Accessory", sellable_id: Accessory.find_by(name: 'Shelf').id, amount: rand(25) },
+  {sku: "tab2", price: 10000, sellable_type: "Accessory", sellable_id: Accessory.find_by(name: 'Table').id, amount: rand(25) },
+  {sku: "thingy42", price: 3400, sellable_type: "Accessory", sellable_id: Accessory.find_by(name: 'Hanging thing').id, amount: rand(25) }
+])
